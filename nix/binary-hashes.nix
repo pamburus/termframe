9b5ca87370e63422f430c278d8f08f7c95866cff
GitHub Action <action@github.com>
2025-09-29 18:13:10 +0000

    build(nix): update binary hashes for v0.6.4

--- nix/binary-hashes.nix
+++ nix/binary-hashes.nix
@@ -1,8 +1,8 @@
 {
-  "0.6.3" = {
-    "termframe-linux-arm64-musl.tar.gz" = "sha256-g03iaSRc3pWhZccxTVT8lqC4WMa+a+wJ6h/KFkskGwo=";
-    "termframe-linux-x86_64-musl.tar.gz" = "sha256-ANilvaUy0sWGP6F1wLmrOTyiBye/3rfFDkj/oelVAm0=";
-    "termframe-macos-arm64.tar.gz" = "sha256-ogCB6RBkAY1P2/tWvgC/dJtVmUhUXi8NoHpvnhskiGg=";
-    "termframe-macos-x86_64.tar.gz" = "sha256-OaMCw3zwBwSwVhlZSqOFyNFrKz2ortDO0rNbrt1wozg=";
+  "0.6.4" = {
+    "termframe-linux-arm64-musl.tar.gz" = "sha256-uUP4t/VgEM2rLfkRiyBNYW/OUeYriqLPkybRGtA36DY=";
+    "termframe-linux-x86_64-musl.tar.gz" = "sha256-X0v1+D9UN1Af86TNJLxwST+JqTuJOGlrCpPrc4jad/E=";
+    "termframe-macos-arm64.tar.gz" = "sha256-jOgWVAz7KnAm7OTnu+Wxo+qcW3qsCrGA18csP7MILNA=";
+    "termframe-macos-x86_64.tar.gz" = "sha256-jCCfPU9fwVDzpel8S5EeeLRrdEEdLeRP/Pj6OY6lXK0=";
   };
 }
